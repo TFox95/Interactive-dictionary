0e37327c6d30659d63ee4a57df83b28b03291811
Thomas Fox <TFox95@aol.com>
2020-12-25 16:03:44 -0600

    Update 1stapp.py

--- 1stapp.py
+++ 1stapp.py
@@ -4,58 +4,38 @@ from difflib import get_close_matches
 #This is loading the json file
 data = json.load(open("data.json", "r"))
 
-#This is a function to find a word in the json file
 def translate(w):
-	#making sure w is always constantly a lower input
-	w = w.lower()
-	#If the word is in the file return the value
-	if w in data:
-		 return data[w]
+    """This is a function to find a word in the json file""" # <--- use docstrings
+    w = w.lower()                                              # <--- also cleaned up this little block before matches
+    if w in data:
+        return data[w]
+
+    matches = get_close_matches(w, data.keys()) # <--- pulled this out of all the lower parts so this is only being called once instead of 3 times. lowers complexity and runtime
+    if len(matches) > 0:
+        print(f'The Word you spelled was not in the dictionary. I think you meant to say this {matches[0]}?') # <--- using fstrings because they are better
+        uw = str(matches[0])
+        answer = input("Did you want to enter {} instead? y/n: ".format(str(matches[0]))) # <--- another superior option for string formatting but not as good as fstrings
+        if answer.lower() == "y":                                                    # <--- cleaned up formatting from here....         
+            return data[uw]
+        elif answer.lower() == "n":
+            return print("That Word Doesn't Exist!")
+        else:
+            return print("Input was not an option please double check it.")
+    else:
+        return "\nThe word you typed doesn't exist! Please double check it"          # <--- .... to here
 
 
-	#if a Word is not in the file return spellcheck warning	
-	elif len(get_close_matches(w, data.keys())) > 0:
+def main(): # <--- created a main class for the items that were just floating, this is better etiquette
+    word = input("Enter a Word: ") # <--- changed the value from w to word, try to have variables be as descriptive as possible, but at the same time simple
+    output = translate(word)
+    if type(output) == list:                        # <--from here
+        for i in output:
+            print("\n%s" % i, sep="\n")
+    elif type(output) == str:
+        print(output)
+    else:
+        print("Error 404")                          # <--to here all I did was change the formatting
 
-	 print("The Word you spelled was not in the dictionary. I think you meant to say this %s?" % (get_close_matches(w, data.keys())[0])) 
 
-	 uw = str((get_close_matches(w, data.keys())[0]))
- 	
- 	#Now we are asking for a y for yes or a n for no to still execute the program
-	 answer = input("Did you want to enter %s instead? y/n: " % str(get_close_matches(w, data.keys())[0]))
-
-	 if answer.lower() == "y":
-
-	 		#now we need to take the crrected word and make it the variable 		 
-	 	return data[uw]
-
-	 elif answer.lower() == "n":
-
-	 	return print("That Word Doesn't Exist!")
-
-	 else:
-
-	 	return print("Input was not an option please double check it.")
-
-	 #Else if there is no possible word return a no such word phrase
-	else:
-		return "\nThe word you typed doesn't exist! Please double check it"
-
-w = input("Enter a Word: ")
-
-#This is to convert the definitions to a variable
-output = translate(w)
-
-#This is a condition to check whether it's an list or string
-if type(output) == list:
-	
-	#This is for if it's an list
-	for i in output:
-		print("\n%s" % i, sep="\n")
-
-#This is for if it's an String
-elif type(output) == str:
-
-	print(output)
-
-else:
-	print("Error 404")
+if __name__ == "__main__": # <--- this is generally used to kick off running the program
+    main()
