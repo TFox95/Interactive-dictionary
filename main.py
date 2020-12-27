e81f4e7982147ebe529a515184111cc5928a9e5d
Thomas Fox <62257263+TFox95@users.noreply.github.com>
2020-12-27 16:01:38 -0600

    v2.5

--- main.py
+++ main.py
@@ -1,10 +1,10 @@
 import translator
 
 def main(): # <--- created a main class for the items that were just floating, this is better etiquette
-    answer = input("Do you want to use voice or text?\n v/t: ")
-    if "v" in answer:
+    answer = input("Do you want to use voice?\n y/n: ")
+    if "y" in answer:
         translator.vtranslation()
-    elif "t" in answer:
+    elif "n" in answer:
         translator.wtranslation()
 if __name__ == "__main__": # <--- this is generally used to kick off running the program
     main()
